c233e85e8da844e2dfd12a05178a94a7b98f0eae
zakaria <zakariabaouali@outlook.com>
2024-06-05 00:08:45 +0100

    expose the redis intance to all the app

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,18 @@
 import express, { Request, Response } from "express";
 import { config } from "./config";
+import productController from "./controllers/Product.controller";
 import { Cashe } from "./redis";
 
 const app = express();
+app.use(express.json());
 const db = new Cashe();
 db.connectTo();
+export { db };
+
+app.use("/api/", productController);
 
 app.get("/check_health", (req: Request, res: Response) => {
   return res.status(200).send("Server working 🚀🚀🚀");
-});
-
-app.get("/data", async (req: Request, res: Response) => {
-  await db.storeCashe("names", `{name : "zakaria" , age : 25}`);
-  return res.status(200).send("ok");
-});
-
-app.get("/data/cashe", async (req: Request, res: Response) => {
-  await db.getCashedData("names");
-  return res.status(200).send("ok");
 });
 
 app.listen(config.APP_PORT, () =>
