dcc436458c6fe5ff63ebcc52776b0ae20f60bb04
zakaria <zakariabaouali@outlook.com>
2024-06-04 18:15:30 +0100

    create simple impl for redis

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const config = {
   APP_PORT: process.env.PORT || 8080,
-  REDIS_PORT: process.env.REDIS_PORT || 6379,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from "express";
 import { config } from "./config";
+import { Cashe } from "./redis";
 
 const app = express();
+const db = new Cashe();
+db.connectTo();
 
 app.get("/check_health", (req: Request, res: Response) => {
   return res.status(200).send("Server working 🚀🚀🚀");
+});
+
+app.get("/data", async (req: Request, res: Response) => {
+  await db.storeCashe("names", `{name : "zakaria" , age : 25}`);
+  return res.status(200).send("ok");
+});
+
+app.get("/data/cashe", async (req: Request, res: Response) => {
+  await db.getCashedData("names");
+  return res.status(200).send("ok");
 });
 
 app.listen(config.APP_PORT, () =>
